87a4264a5422ec6dd2096b2742a39830453e7bd7
emiliano080591 <emilianoruben91@gmail.com>
2021-10-19 16:21:09 -0500

    Clean main branch

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"log"
-)
-
 func main()  {
-	c:=saveData(
-		fetchData(
-			prepareData(
-				generateData(),
-				),
-			),
-		)
-	for data:=range c{
-		log.Printf("Items saved: %v\n",data)
-	}
+
 }
