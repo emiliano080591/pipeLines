1f0148630864081cf367f5aa360c9c8792a39e26
emiliano080591 <emilianoruben91@gmail.com>
2021-10-19 16:26:50 -0500

    Actualizar generator.go, main.go y square.go

--- main.go
+++ main.go
@@ -2,32 +2,18 @@ package main
 
 import "log"
 
+// TODO: Build a Pipeline
+// generator() -> square() -> print
+// set up the pipeline
+
+// run the last stage of pipeline
+// receive the values from square stage
+// print each one, until channel is closed.
 func main()  {
-	for n:=range sq(gen(5,4,7,9,15,11,12,17)){
+	for n:=range square(generator(5,4,7,9,15,11,12,17)){
 		log.Println(n)
 	}
 
 }
 
-func gen(nums ...int) <-chan int {
-	out:=make(chan int,len(nums))
-	go func() {
-		for _,n:=range nums{
-			out<-n
-		}
-		close(out)
-	}()
-	return out
-}
-
-func sq(in <-chan int) <-chan int{
-	out:=make(chan int)
-	go func() {
-		for n:=range in{
-			out<-n*n
-		}
-		close(out)
-	}()
-	return out
-}
 
---
+++ square.go
@@ -0,0 +1,15 @@
+package main
+
+// square - receive on inbound channel
+// square the number
+// output on outbound channel
+func square(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
